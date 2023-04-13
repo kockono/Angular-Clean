cdd38779af840deb931212a48692b4db784a67c3
kockono <kockonoop@gmail.com>
2023-04-13 16:24:21 -0600

    feat: Helpers

---
+++ src/app/helpers/parse-json.ts
@@ -0,0 +1,15 @@
+/**
+ * @author Chris Cobos
+ * @version 1.0.0
+ * @description - Obtiene el elemento guardado en el local storage y lo parsea
+ * 
+ * @param itemName  - Dato a buscar del local storage
+ * @var item        - Variable con los datos parseado
+ * @return { item } - El item ya parseado del dato buscado
+ *
+ * @function {@link_getItemsLocalStorage} - Metodo para obtención del dato parseado
+ */
+export function getItemsLocalStorage(itemName:string):any {
+  let item : any = localStorage.getItem(itemName);
+  return JSON.parse(item);
+}
---
+++ src/app/helpers/validate-form.ts
@@ -0,0 +1,22 @@
+import { FormGroup } from '@angular/forms';
+/**
+ * @author Chris Cobos
+ * @version 1.0.1
+ *
+ * Esta sección permite validar formularios globalmente
+ * @param simpleForm       - Formulario enviado desde el Html
+ * @var simpleForm.invalid - Valida si se respeto lo indicado
+ */
+export function isValidForm(simpleForm:any):boolean {
+  if ( !simpleForm.invalid ) return false;
+    Object.values(simpleForm.controls).forEach((control) => {
+      if (control instanceof FormGroup) {
+        Object.values(control.controls).forEach((control) =>
+          control.markAsTouched()
+        );
+      } else {
+        simpleForm.markAllAsTouched();
+      }
+    });
+    return true;
+}
