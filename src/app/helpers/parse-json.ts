dafe13138edd491dae3b44b17f190544bfe5ece2
kockono <kockonoop@gmail.com>
2023-04-13 17:28:07 -0600

    docs: Documentation update for parseJson

--- src/app/helpers/parse-json.ts
+++ src/app/helpers/parse-json.ts
@@ -3,13 +3,13 @@
  * @version 1.0.0
  * @description - Obtiene el elemento guardado en el local storage y lo parsea
  * 
- * @param itemName  - Dato a buscar del local storage
- * @var item        - Variable con los datos parseado
- * @return { item } - El item ya parseado del dato buscado
+ * @param  {string} itemName  - Dato a buscar del local storage
+ * @var    {string} item      - Variable con los datos parseado
+ * @return {item}             - El item ya parseado del dato buscado
  *
  * @function {@link_getItemsLocalStorage} - Metodo para obtención del dato parseado
  */
 export function getItemsLocalStorage(itemName:string) {
-  const item:string | null = localStorage.getItem(itemName) || "";
+  const item:string = localStorage.getItem(itemName) || "";
   return JSON.parse(item);
 }
